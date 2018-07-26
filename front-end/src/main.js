5a044390c19e2c6f1c00b6a82c1d19d56634b590
sc8ing <16541352+sc8ing@users.noreply.github.com>
2018-07-26 13:23:05 -0400

    single experiment app

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -15,90 +15,12 @@ import ReactDOM from 'react-dom'; // eslint-disable-line no-unused-vars
 import FastClick from 'fastclick';
 import { Route, Switch, BrowserRouter as Router, browserHistory } from 'react-router-dom'; // eslint-disable-line no-unused-vars
 import Sktc from './quizzes/sktc/index';
-import ErrorPage from './pages/error/index';
+const ErrorPage = () => (<h1>404</h1>);
 
-
-
-//import { Provider } from 'react-redux'; // eslint-disable-line no-unused-vars
-//import { syncHistoryWithStore } from 'react-router-redux';
-//import { rootReducer } from './reducers/index';
-//import thunkMiddleware from 'redux-thunk';
-//import { compose, createStore, applyMiddleware } from 'redux';
-//import { hot } from 'react-hot-loader';
-//import { logger } from 'redux-logger';
-
-// Pages / Routing
-/*
-import HomePage from './pages/home/index';
-import About from './pages/about/index';
-import Quizzes from './pages/quizzes/index';
-// kid tester specific site
-import TakeQuiz from './pages/quizzes/TakeQuiz.js';
-import Updates from './pages/updates/index';
-import Container from './pages/containers/container'; // eslint-disable-line no-unused-vars
-import Dashboard from './pages/dashboard/index'; // eslint-disable-line no-unused-vars
-import Forum from './pages/forum/index';
-import Admin from './pages/admin/index';
-*/
-
-//import { CONFIG } from './config.js';
-//import Auth from './core/auth';
-
-
-//export default function configureStore(initialState) {
-//	const middleWares = [ thunkMiddleware /*, logger*/ ];
-//	return createStore(
-//		rootReducer,
-//		initialState,
-//		compose(applyMiddleware.apply(this, middleWares))
-//	);
-//}
-
-//const store = configureStore();
-//const history = syncHistoryWithStore(browserHistory, store);
-
-//const App = hot(module)( () => ( // eslint-disable-line no-unused-vars
-//	<Provider store={store}>
-//		{/*		<Router onUpdate={() => window.scrollTo(0, 0)} history={history}> */}
 const App = () => (
 			<Router>
 				<Switch>
 					<Route exact path='/' component={Sktc} />
-					{/*
-					<Route exact path='/' component={HomePage}
-						auth={CONFIG.auth}
-						showForum={CONFIG.forum}
-					/>
-					<Route path="/quizzes" component={Quizzes}
-						auth={CONFIG.auth}
-						showForum={CONFIG.forum}
-					/>
-					<Route path="/admin" component={Admin}
-						auth={CONFIG.auth}
-						showForum={CONFIG.forum}
-					/>
-					<Route path="/about" component={About}
-						auth={CONFIG.auth}
-						showForum={CONFIG.forum}
-					/>
-					<Route path="/updates" component={Updates}
-						auth={CONFIG.auth}
-						showForum={CONFIG.forum}
-					/>
-					{ CONFIG.forum && (
-						<Route path='/forum' component={Forum}
-							auth={CONFIG.auth}
-							showForum={CONFIG.forum}
-						/>
-					)}
-					{ CONFIG.auth && (
-						<Route path="/dashboard" component={Dashboard}
-							auth={CONFIG.auth}
-							showForum={CONFIG.forum}
-						/>
-					)}
-					// kid tester specific site*/}
-					<Route path="/error" component={ErrorPage} />
 					<Route path="*" component={ErrorPage} />
 				</Switch>
 			</Router>
